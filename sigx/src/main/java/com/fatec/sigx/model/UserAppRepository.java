b39f306715bf8ea35e679054ecf7889160a5746c
almei <almei@laptopp>
2022-09-18 15:46:23 -0300

    Inclusao do CRUD de Cliente - cap06

---
+++ sigx/src/main/java/com/fatec/sigx/model/UserAppRepository.java
@@ -0,0 +1,10 @@
+package com.fatec.sigx.model;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserAppRepository extends JpaRepository<UsuarioApp, Long>{
+	Optional<UsuarioApp> findByUserName(String usarName);
+}
